f3a4020ed84e89c44e50334cebd7fb5e2567a442
Austin Croucher <austin.croucher@gmail.com>
2023-06-02 18:07:18 -0400

    gallery

--- src/components/projects/projects.jsx
+++ src/components/projects/projects.jsx
@@ -4,9 +4,14 @@ import projects from '../../assets-portfolio-qr/projects1.png'
 import retroRumble from '../../assets-portfolio-qr/RetroRumble.jpeg'
 import retroRumbleVideo from '../../assets-portfolio-qr/RetroRumbleVid.mp4'
 import { useState } from 'react'
+import retro1 from '../../assets-portfolio-qr/Retro1.png'
+import retro2 from '../../assets-portfolio-qr/retro2.png'
+import retro3 from '../../assets-portfolio-qr/retro3.png'
+
 
 function ProjectsPage() {
     const [showVideo, setShowVideo] = useState(false);
+    const [showGallery, setShowGallery] = useState(false);
 
     return (
         <>
@@ -24,7 +29,7 @@ function ProjectsPage() {
                         other gamers from around the world to see who's the best.
                     </p>
                     <button className='link--button' onClick={() => setShowVideo(true)}>Video Demo</button>
-                    <button className='link--button'>Gallery</button>
+                    <button className='link--button' onClick={() => setShowGallery(true)}>Gallery</button>
                 </div>
                 {showVideo && (
                     <>
@@ -35,6 +40,17 @@ function ProjectsPage() {
                                 Your browser does not support the video tag.
                             </video>
                             <button className='link--button' onClick={() => setShowVideo(false)}>Close</button>
+                        </div>
+                    </>
+                )}
+                {showGallery && (
+                    <>
+                        <div className='gallery-overlay' onClick={() => setShowGallery(false)}></div>
+                        <div className='gallery'>
+                            <img src={retro1} alt="Gallery" />
+                            <img src={retro2} alt="Gallery" />
+                            <img src={retro3} alt="Gallery" />
+                            <button className='link--button' onClick={() => setShowGallery(false)}>Close</button>
                         </div>
                     </>
                 )}
