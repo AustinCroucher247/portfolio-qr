2af176ce7b79ec9640d50c731272720096588efc
Austin Croucher <austin.croucher@gmail.com>
2023-06-05 15:21:39 -0400

    header style

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,32 +1,35 @@
+import React, { useState } from 'react';
 import './header.scss';
 import { Link } from 'react-router-dom';
 
 function Header() {
+    const [isOpen, setIsOpen] = useState(false);
+
     return (
         <>
             <section className="top-nav">
-                <input id="menu-toggle" type="checkbox" />
+                <input id="menu-toggle" type="checkbox" checked={isOpen} onChange={() => setIsOpen(!isOpen)} />
                 <label className='menu-button-container' htmlFor="menu-toggle">
                     <div className='menu-button'></div>
                 </label>
                 <ul className="menu">
                     <li className='nav--text'>
-                        <Link to={'/'} style={{ textDecoration: 'none', color: 'inherit' }}>
+                        <Link to={'/'} style={{ textDecoration: 'none', color: 'inherit' }} onClick={() => setIsOpen(false)}>
                             Home
                         </Link>
                     </li>
                     <li className='nav--text'>
-                        <Link to={'./nonInteractive/projects'} style={{ textDecoration: 'none', color: 'inherit' }}>
+                        <Link to={'./nonInteractive/projects'} style={{ textDecoration: 'none', color: 'inherit' }} onClick={() => setIsOpen(false)}>
                             Projects
                         </Link>
                     </li>
                     <li className='nav--text'>
-                        <Link to={'/contact'} style={{ textDecoration: 'none', color: 'inherit' }}>
+                        <Link to={'/contact'} style={{ textDecoration: 'none', color: 'inherit' }} onClick={() => setIsOpen(false)}>
                             Contact
                         </Link>
                     </li>
                     <li className='nav--text'>
-                        <Link to={'/interactive'} style={{ textDecoration: 'none', color: 'inherit' }}>
+                        <Link to={'/interactive'} style={{ textDecoration: 'none', color: 'inherit' }} onClick={() => setIsOpen(false)}>
                             Interactive
                         </Link>
                     </li>
